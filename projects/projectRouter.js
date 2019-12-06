374ca6de437c93ddeed8897e953cf38840cc2f2e
Sara De La Cruz <54650844+Sara-DLC@users.noreply.github.com>
2019-12-06 11:20:30 -0600

    get/:id and middleware implemented

--- projects/projectRouter.js
+++ projects/projectRouter.js
@@ -17,4 +17,42 @@ router.get('/', (req, res) => {
     });
 });
 
+router.get('/:id', validateId,  (req, res) => {
+const id = req.params.id;
+db.getProjectActions(id)
+.then(userById => {
+    userById ? res.status(200).json(userById) : res.status(404).json({ message: "The project with the specified ID does not exist." });
+})
+.catch(error => {
+    console.log(error);
+    res.status(500).json({ error: "The project information could not be retrieved." });
+});
+});
+
+
+
+//middleware
+
+function validateId (req, res, next) {
+    const id = req.params.id;
+    db.get(id)
+    .then(project => {
+        if(project) {
+            req.project = project
+            next()
+        }
+        else {
+            res.status(400).json({
+                message: `invalid project id`
+            })
+        }
+    })
+    .catch(error => {
+        res.status(500).json({ error: `Project does not exist`, error
+        });
+    });
+};
+
+
+
 module.exports = router;
